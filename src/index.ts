9a5fbde4894b8fdf09599cea494652a0a088d254
Pantha Acharjee <freelancerpantha@gmail.com>
2025-06-10 01:11:26 +0600

    First commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import app from './app';
 
-const server = app.listen(4000, () => {
+const dotenv = require("dotenv")
+
+
+dotenv.config({path:"./config/config.env"})
+
+const server = app.listen(process.env.PORT, () => {
   console.log(`Server running on port 4000`);
 });
 
